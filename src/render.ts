62ce9cf49d439dad296aa7b832f77968dee92466
andrewkolos <andrewrkolos@gmail.com>
2017-10-16 21:03:34 -0400

    add animation, table, and grass

--- src/render.ts
+++ src/render.ts
@@ -6,6 +6,9 @@ import {createTickWrapper} from "./util/tickwrapper";
 import {Card, Deck, FACE, SUIT} from "./deck";
 import {AxisHelper, Object3D} from "three";
 
+declare let TWEEN: any;
+
+
 export class Stage {
 
     camera: THREE.PerspectiveCamera;
@@ -28,23 +31,30 @@ export class Stage {
         /* this.scene = new Physijs.Scene();
          this.scene.setGravity(new THREE.Vector3(0, -30, 0));*/
         this.scene = new THREE.Scene();
+        this.scene.fog = new THREE.Fog(0xffffff, 0.1, 50);
         this.initLights();
         this.initCamera();
         this.initRenderer();
         this.createCards(SUIT.CLUBS, SUIT.DIAMONDS, SUIT.HEARTS);
         new OrbitControls(this.camera, this.renderer.domElement);
         this.scene.add(new AxisHelper(3));
+
+
+        this.initTableObject();
+
+
+        this.initGround();
     }
 
     start = () => {
         this.rendering = true;
-        requestAnimationFrame(() => this.render());
+        requestAnimationFrame((time) => this.render(time));
     };
 
-    render = () => {
+    render = (time) => {
         if (this.rendering) {
-            console.log('rendering');
-            requestAnimationFrame(() => this.render());
+            requestAnimationFrame((time) => this.render(time));
+            TWEEN.update(time);
             this.update();
             this.renderer.render(this.scene, this.camera);
         }
@@ -59,35 +69,52 @@ export class Stage {
     }
 
     private initLights() {
-        let dirLight = new THREE.DirectionalLight(0xffffff, 1.2);
-        dirLight.position.set(0, 10, -15);
-        dirLight.castShadow = true; // expensive
-        dirLight.shadow.camera.near = 0.1;
-        dirLight.shadow.camera.far = 60;
-        dirLight.shadow.camera.left = -25;
-        dirLight.shadow.camera.right = 25;
-        dirLight.shadow.camera.top = 16;
-        dirLight.shadow.camera.bottom = -16;
-        dirLight.shadow.mapSize.width = 1024 * 2;
-        dirLight.shadow.mapSize.height = 1024 * 2;
-        this.scene.add(dirLight);
+        let dirLight1 = new THREE.DirectionalLight(0xffffff, 0.4);
+        dirLight1.position.set(-7, 30, 15);
+        dirLight1.castShadow = true; // expensive
+        dirLight1.shadow.camera.near = 10;
+        dirLight1.shadow.camera.far = 70;
+        dirLight1.shadow.camera.left = -9;
+        dirLight1.shadow.camera.right = 9;
+        dirLight1.shadow.camera.top = 6;
+        dirLight1.shadow.camera.bottom = -6;
+        dirLight1.shadow.mapSize.width = 1024 * 2;
+        dirLight1.shadow.mapSize.height = 1024 * 2;
+        this.scene.add(dirLight1);
+
+        let dirLight2 = new THREE.DirectionalLight(0xffffff, 0.3);
+        dirLight2.position.set(7, 30, -15);
+        dirLight2.castShadow = true; // expensive
+        dirLight2.shadow.camera.near = 10;
+        dirLight2.shadow.camera.far = 70;
+        dirLight2.shadow.camera.left = -9;
+        dirLight2.shadow.camera.right = 9;
+        dirLight2.shadow.camera.top = 6;
+        dirLight2.shadow.camera.bottom = -6;
+        dirLight2.shadow.mapSize.width = 1024 * 2;
+        dirLight2.shadow.mapSize.height = 1024 * 2;
+        this.scene.add(dirLight2);
+        this.scene.add(new THREE.CameraHelper(dirLight2.shadow.camera));
+        this.scene.add(new THREE.DirectionalLightHelper(dirLight2));
 
         let hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.3);
         hemiLight.color.setHSL(0.6, 1, 0.6);
         hemiLight.groundColor.setHSL(0.095, 1, 0.75);
-        hemiLight.position.set(0, 100, 0);
+        hemiLight.position.set(0, 10, 0);
         this.scene.add(hemiLight);
 
         let ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
         this.scene.add(ambientLight);
 
-        this.scene.add(new THREE.DirectionalLightHelper(dirLight));
-        this.scene.add(new THREE.CameraHelper(dirLight.shadow.camera));
+        this.scene.add(new THREE.DirectionalLightHelper(dirLight1));
+
+        this.scene.add(new THREE.CameraHelper(dirLight1.shadow.camera));
+
     }
 
     private initCamera() {
-        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
-        this.camera.position.set(0, 15, -10);
+        this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 10000);
+        this.camera.position.set(0, 5.5, 5);
         this.camera.lookAt(new THREE.Vector3(0, 0, 0));
     }
 
@@ -105,6 +132,37 @@ export class Stage {
         });
 
         document.body.appendChild(this.renderer.domElement);
+    }
+
+    private initTableObject() {
+        let object = this.resources.table;
+        let applyTexture = (obj) => {
+            obj.receiveShadow = true;
+            if (obj instanceof THREE.Mesh) {
+                console.log('jojjing');
+                (<THREE.MeshPhongMaterial>obj.material).map = this.resources.woodTexture;
+            }
+            if (obj instanceof THREE.Object3D)
+                obj.children.forEach(c => applyTexture(c));
+        };
+        console.log(this.resources.woodTexture);
+        console.log(object);
+        applyTexture(object.children[0]);
+        object.scale.set(4, 1, 3.2);
+        object.position.set(0, -1.65, 0);
+        object.receiveShadow = true;
+        this.scene.add(object);
+    }
+
+    private initGround() {
+        let geometry = new THREE.PlaneGeometry(100, 100);
+        let material = new THREE.MeshPhongMaterial({map: this.resources.grassTexture});
+        let mesh = new THREE.Mesh(geometry, material);
+        mesh.rotateX(-Math.PI/2);
+        mesh.position.set(0, -1.65, 0);
+        mesh.receiveShadow = true;
+
+        this.scene.add(mesh);
     }
 
     private createCards(boardSuit: SUIT, playerSuit: SUIT, computerSuit: SUIT) {
@@ -131,25 +189,33 @@ export class Stage {
         this.computerHand = buildHand(computerSuit);
         this.computerBoard = new PhysicalDeck();
 
-        // MAKE SURE THIS WORKS
-        /*const allDecks = {...this.playerHand, ...this.computerHand, ...this.neutralBoard};
-        allDecks.map.forEach((value: THREE.Object3D, key: Card) => {
-            this.scene.add(value);
-        });*/
-        Array.from(this.playerHand.map.values()).forEach(v=> this.scene.add(v));
+        Array.from(this.playerHand.map.values()).forEach(v => this.scene.add(v));
+        Array.from(this.neutralBoard.map.values()).forEach(v => this.scene.add(v));
+        Array.from(this.computerHand.map.values()).forEach(v => this.scene.add(v));
 
-        // make this manual later on
-        this.dealCards(Array.from(this.playerHand.map.values()), new THREE.Vector3(0,0,0), new THREE.Euler(0,0,0));
+        setTimeout(() => {// make this manual later on
+            this.dealCards(Array.from(this.playerHand.map.values()), new THREE.Vector3(0, 0.4, 3), new THREE.Vector3(-.2, .01, 0), new THREE.Euler(-Math.PI / 3, 0, 0));
+            this.dealCards(Array.from(this.neutralBoard.map.values()), new THREE.Vector3(0, 0, 0), new THREE.Vector3(0.1, 0, 0), new THREE.Euler(-Math.PI / 2, 0, 0));
+            this.dealCards(Array.from(this.computerHand.map.values()), new THREE.Vector3(0, 0, -3), new THREE.Vector3(0.1, 0, 0), new THREE.Euler(Math.PI / 2, 0, 0));
+        }, 1);
+
     }
 
-    private dealCards(cards: THREE.Object3D[], pos: THREE.Vector3, rotation: THREE.Euler) {
-        let padding = 0.1;
-        let start = -(cards.length + (cards.length * padding)) / 2;
+    private dealCards(cards: THREE.Object3D[], pos: THREE.Vector3, padding: THREE.Vector3, rotation: THREE.Euler) {
+
+        let start = new THREE.Vector3(-(cards.length + (cards.length * padding.x)) /2.0 + 0.6, pos.y, pos.z);
 
         let currentPosition = start;
         cards.forEach(card => {
-            card.position.set(pos.x + currentPosition, pos.y, pos.z);
-            currentPosition += 1 + padding;
+            let position = {x: card.position.x, y: card.position.y, z: card.position.z};
+            let target = {x: pos.x + currentPosition.x, y: currentPosition.y, z: currentPosition.z};
+            let tween = new TWEEN.Tween(position).to(target, 1000)
+                .easing(TWEEN.Easing.Quadratic.Out)
+                .onUpdate(function () {
+                    card.position.set(position.x, position.y, position.z);
+                }).start();
+            card.rotation.set(rotation.x, rotation.y, rotation.z);
+            currentPosition = currentPosition.add(new THREE.Vector3(1 + padding.x, padding.y, padding.z));
         });
     }
 }
@@ -199,23 +265,39 @@ function createCardObject(resources: ResourceManager, card: Card): THREE.Object3
     let replaceStringWithNumber = (str: string) => {
         // not exactly efficiently but nice and short
         return str.replace('one', '1').replace('two', '2').replace('three', '3').replace('four', '4')
-            .replace('five', '5').replace('six', '6').replace('seven','7').replace('eight','8').replace('nine','9').replace('ten','10');
+            .replace('five', '5').replace('six', '6').replace('seven', '7').replace('eight', '8').replace('nine', '9').replace('ten', '10');
     };
 
     let imageName = replaceStringWithNumber(FACE[card.face].toLowerCase()) + "_of_" + SUIT[card.suit].toLowerCase();
     let frontGeometry = new THREE.PlaneGeometry(500 / 500, 726 / 500);
     let backGeometry = frontGeometry.clone();
     let frontTexture = resources.cardTextures[imageName];
+    frontTexture.minFilter = THREE.LinearFilter;
+    frontTexture.magFilter = THREE.LinearFilter;
 
     console.log(resources.frontSideAlpha);
 
-    let frontMaterial = new THREE.MeshPhongMaterial({alphaMap: resources.frontSideAlpha, alphaTest: 0.9, map: frontTexture, side:THREE.FrontSide});
-    let backMaterial = new THREE.MeshPhongMaterial({alphaMap: resources.backSideAlpha, alphaTest: 0.9, map: resources.cardBackTexture, side:THREE.BackSide});
+    let frontMaterial = new THREE.MeshPhongMaterial({
+        alphaMap: resources.frontSideAlpha,
+        alphaTest: 0.9,
+        map: frontTexture,
+        side: THREE.FrontSide
+    });
+    let backMaterial = new THREE.MeshPhongMaterial({
+        alphaMap: resources.backSideAlpha,
+        alphaTest: 0.9,
+        map: resources.cardBackTexture,
+        side: THREE.BackSide
+    });
 
     let object = new THREE.Object3D();
     let frontMesh = new THREE.Mesh(frontGeometry, frontMaterial);
+    frontMesh.castShadow = true;
+    let backMesh = new THREE.Mesh(backGeometry, backMaterial);
+    backMesh.castShadow = true;
     object.add(frontMesh);
-    object.add(new THREE.Mesh(backGeometry, backMaterial));
-
+    object.add(backMesh);
+    object.castShadow = true;
+    object.receiveShadow = true;
     return object;
 }
--- src/resources.ts
+++ src/resources.ts
@@ -11,6 +11,9 @@ export class ResourceManager {
     cardBackTexture: THREE.Texture;
     frontSideAlpha: THREE.Texture;
     backSideAlpha: THREE.Texture;
+    woodTexture: THREE.Texture;
+    grassTexture: THREE.Texture;
+    table: THREE.Object3D;
 
     constructor() {
         this.textureLoader = new THREE.TextureLoader();
@@ -20,14 +23,26 @@ export class ResourceManager {
         return new Promise((resolve => {
             //console.log(ResourceManager.promisifyLoadingTexture(this.textureLoader,'images/board.png'));
             let promises = [promisifyLoadingTexture(this.textureLoader, 'images/board.png'), this.cardPromise(), this.cardBackPromise(),
-                            promisifyLoadingTexture(this.textureLoader, 'images/card/frontside_alpha.png'),
-                            promisifyLoadingTexture(this.textureLoader, 'images/card/backside_alpha.png')];
+                promisifyLoadingTexture(this.textureLoader, 'images/card/frontside_alpha.png'),
+                promisifyLoadingTexture(this.textureLoader, 'images/card/backside_alpha.png'),
+                promisifyLoadingTexture(this.textureLoader, 'images/wood.png'),
+                promisifyLoadingTexture(this.textureLoader, 'images/grass.png'),
+                this.tableModelPromise()];
             Promise.all(promises).then((values: any[]) => {
                 this.boardTexture = values[0][1];
                 this.cardTextures = values[1];
                 this.cardBackTexture = values[2];
                 this.frontSideAlpha = values[3][1];
                 this.backSideAlpha = values[4][1];
+                this.woodTexture = values[5][1];
+                this.woodTexture.offset.set(0, 0);
+                this.woodTexture.repeat.set(24, 24);
+                this.woodTexture.wrapS = this.woodTexture.wrapT = THREE.RepeatWrapping;
+                this.grassTexture = values[6][1];
+                this.grassTexture.offset.set(0, 0);
+                this.grassTexture.repeat.set(48, 48);
+                this.grassTexture.wrapS = this.grassTexture.wrapT = THREE.RepeatWrapping;
+                this.table = values[7];
                 this.loaded = true;
                 resolve();
             })
@@ -45,7 +60,7 @@ export class ResourceManager {
                 success: function (data) {
                     $(data).find("a:contains(.png)").each((index, element) => {
                         let filename = (<HTMLAnchorElement>element).href.replace(window.location.host, "").replace("http:///", "");
-                        promises.push(promisifyLoadingTexture(loader,dir + '/' + filename));
+                        promises.push(promisifyLoadingTexture(loader, dir + '/' + filename));
                     });
                     Promise.all(promises).then((values: any[]) => {
                         values.forEach((value: [string, THREE.Texture]) => {
@@ -67,6 +82,14 @@ export class ResourceManager {
                 resolve(texture);
             })
         })
+    };
+
+    tableModelPromise = () => {
+        return new Promise<THREE.Object3D>(resolve => {
+            (new THREE.ObjectLoader()).load('models/lowtable.json', object => {
+                resolve(object);
+            });
+        });
     }
 }
 
