aebc452f7e16bba1c1b3a106530cf009c1c3bd81
Andrew Kolos <andrewrkolos@gmail.com>
2020-06-06 14:58:07 -0400

    rename shuffle func to shuffleInPlace

--- src/card/shuffle.ts
+++ src/card/shuffle.ts
@@ -2,10 +2,12 @@
  * Shuffles an array in-place using the Fisher-Yates shuffling algorithm.
  * @param arr
  */
-export function shuffle<T>(arr: T[]) {
+export function shuffleInPlace<T>(arr: T[]): T[] {
   arr.forEach((_value: T, i: number) => {
     swap(i, i + randomUpTo(arr.length - i));
   });
+  return arr;
+
   function swap(i: number, j: number) {
     const temp = arr[i];
     arr[i] = arr[j];
